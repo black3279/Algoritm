108813b4922c7aaa4e494e0dba47efedc8ec9c67
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-09 18:38:19 +0900

    Add files via upload
    Fraction sort problem, which I solved about 4 month ago.

--- main.c
+++ main.c
@@ -1,76 +1,48 @@
 #if 01
 
-#include <stdio.h> 
-
-
-
-int n;
-
-int x[100 + 10], y[100 + 10];
-
-int area;
-
-int arr[101][101] = { 0, };
-
-
-void paper(int C, int R)
-{
-	int i, j;
-
-	for (i = R; i<R + 10; i++)
-	{
-		for (j = C; j<C + 10; j++)
-		{
-			arr[i][j]++;
-		}
-	}
-
-}
-
+#include <stdio.h>
 
 int main(void)
 
 {
+	int mo, ja;
+	int i;
+	int n;
 
-	int i, j;
+	int clear[30000][2] = { 0, };
 
-	// 입력받는 부분
 
 	scanf("%d", &n);
 
-	for (i = 0; i<n; i++)
+	printf("0/1\n");
 
+	for (mo = 2; mo <= n; mo++)
 	{
-
-		scanf("%d %d", &x[i], &y[i]);
-
-	}
-
-
-
-	// 여기서부터 작성
-	for (i = 0; i<n; i++)
-	{
-		paper(x[i], y[i]);
-	}
-
-	for (i = 1; i <= 100; i++)
-	{
-		for (j = 1; j <= 100; j++)
+		for (ja = 1; ja <= mo - 1; ja++)
 		{
-			if (arr[i][j] >= 1) area++;
+			if ((clear[(30000 * ja) / mo][0] == 0))
+			{
+				clear[(30000 * ja) / mo][0] = ja;
+				clear[(30000 * ja) / mo][1] = mo;
+			}
 		}
 	}
 
-	// 출력하는 부분
+	for (i = 0; i<30000; i++)
+	{
+		if (clear[i][0] != 0)
+		{
+			printf("%d/%d\n", clear[i][0], clear[i][1]);
+		}
+	}
 
-	printf("%d", area);
 
+	printf("1/1");
 
+	// 여기서부터 작성
 
 	return 0;
 
 }
-
 
 #endif
