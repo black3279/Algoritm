7914b91d05f3e831a3c289cd386aa3e4377c72e0
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-09 18:47:04 +0900

    Add files via upload
    Solving Peg Solitaire algorithm problem.

--- main.c
+++ main.c
@@ -1,99 +1,175 @@
 #if 01
 #include <stdio.h>
 #include <string.h>
+#define INF 0x7fff0000
 
-int visit[1010];
-int one[1010][31];
-int que[1010];
-int wp, rp;
-int start, end;
-int N, K;
+typedef struct{
+	int y;
+	int x;
+	int flag;
+}location;
 
+int my, mx;
+int map[6][10];
+int init[6][10];
+int pin_num;
 
-int abs(int num)
-{
-	return (num>0) ? num : -num;
-}
+int min_cnt;
+int min_move;
 
+location pin[9];
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { -1, 0, 1, 0 };
 
-void BFS(void)
-{
-	int out, i, j;
-	int cnt = 0;
-	wp = rp = 0;
-
-	que[wp++] = start;
-	visit[start] = -1;
-
-	while (wp>rp)
-	{
-		out = que[rp++];
-
-		for (i = 1; i <= N; i++)
-		{
-			if (visit[i] != 0 || out == i) continue;
-			cnt = 0;
-
-			for (j = 1; j <= K; j++)
-			{
-				if (one[out][j] != one[i][j]) cnt++;
-				if (cnt == 2) break;
-			}
-
-			if (cnt == 1)
-			{
-				visit[i] = out;
-				if (i == end) return;
-				que[wp++] = i;
-			}
-		}
-	}
-
-}
 
 void input(void)
 {
 	int i, j;
-	int temp;
-	scanf("%d %d", &N, &K);
+	char temp;
 
-	for (i = 1; i <= N; i++)
+	for (i = 1; i <= my; i++)
 	{
-		for (j = 1; j <= K; j++)
+		for (j = 1; j <= mx; j++)
 		{
-			scanf("%1d", &one[i][j]);
+			scanf(" %c", &temp);
+			if (temp == '#') map[i][j] = 9;
+			if (temp == '.') map[i][j] = 0;
+			if (temp == 'o')
+			{
+				pin[pin_num].y = i;
+				pin[pin_num].x = j;
+				map[i][j] = pin_num;
+				pin_num++;
+			}
 		}
 	}
 
-	scanf("%d %d", &start, &end);
+	pin_num--;
 }
+
+int check_final(void)
+{
+	int i, j;
+	int nx, ny;
+	int nnx, nny;
+	int sx, sy;
+	int cnt = 0;
+
+	for (i = 1; i <= pin_num; i++)
+	{
+		sx = pin[i].x;
+		sy = pin[i].y;
+
+		if (pin[i].flag == 0)
+		{
+			for (j = 0; j<4; j++)
+			{
+				nx = sx + dx[j];
+				ny = sy + dy[j];
+
+				nnx = nx + dx[j];
+				nny = ny + dy[j];
+
+				if (map[ny][nx]>0 && map[ny][nx]<9)
+				{
+					if (nnx<1 || nnx>mx || nny<1 || nny>my) continue;
+					return 0;
+				}
+			}
+		}
+	}
+
+	return 1;
+}
+
+void P(void)
+{
+	int i, j;
+
+	for (i = 1; i <= my; i++)
+	{
+		for (j = 1; j <= mx; j++)
+		{
+			printf("%d ", map[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void DFS(int L, int cnt)
+{
+	int i, j;
+	int nx, ny, sx, sy;
+	int nnx, nny;
+	int delete_idx;
+	int backup[6][10];
+	if (cnt<min_cnt)
+	{
+		//P();
+		min_cnt = cnt;
+		min_move = pin_num - min_cnt;
+	}
+
+	for (i = 1; i <= pin_num; i++)
+	{
+		if (pin[i].flag == 0)
+		{
+			sx = pin[i].x;
+			sy = pin[i].y;
+			for (j = 0; j<4; j++)
+			{
+				nx = sx + dx[j];
+				ny = sy + dy[j];
+
+				nnx = nx + dx[j];
+				nny = ny + dy[j];
+				if (nnx<1 || nny<1 || nnx>mx || nny>my) continue;
+				if (map[ny][nx] != 0 && map[ny][nx] != 9 && map[nny][nnx] == 0)
+				{
+					delete_idx = map[ny][nx];
+					map[ny][nx] = 0;
+					map[nny][nnx] = i;
+					map[sy][sx] = 0;
+					pin[i].y = nny;
+					pin[i].x = nnx;
+					pin[delete_idx].flag = 1;
+					DFS(L + 1, cnt - 1);
+					pin[i].y = sy;
+					pin[i].x = sx;
+					map[nny][nnx] = 0;
+					map[sy][sx] = i;
+					map[ny][nx] = delete_idx;
+					pin[delete_idx].flag = 0;
+				}
+			}
+		}
+	}
+}
+
 
 int main(void)
 {
-	int i = 0;
-	int cnt = 1;
-	int best[1010];
-
-	input();
-
-	BFS();
-
-	if (visit[end] == 0)
+	int T, tc;
+	scanf("%d", &T);
+	my = 5;
+	mx = 9;
+	for (tc = 1; tc <= T; tc++)
 	{
-		printf("-1");
-		return 0;
+		pin_num = 1;
+		memset(pin, 0, sizeof(pin));
+		input();
+		//P();
+		min_cnt = pin_num;
+		min_move = 0;
+		DFS(0, pin_num);
+
+		printf("%d %d\n", min_cnt, min_move);
+
 	}
-
-	best[i] = end;
-
-	for (;;)
-	{
-		if (visit[best[i]] == -1) break;
-		best[cnt++] = visit[best[i++]];
-	}
-	for (i; i >= 0; i--) printf("%d ", best[i]);
-
 	return 0;
 }
 
+
 #endif
+
