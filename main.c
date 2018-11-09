21c5ba55578e7520e952ae9702ed494a0637cd8c
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-09 18:56:42 +0900

    Add files via upload
    It was a BFS algorithm problem. You can jump like knight of chess (Horse Jump) or walk one step (Monkey Walk).
    You should start at (x, y) = (1, 1) and arrive to (x, y) = (Max of map y, Max of map x) with minimum move.
    You can use Horse Jump less than K.
    My algorithm calculate and print minimum move (Horse Jump + Monkey Walk) at console.

--- main.c
+++ main.c
@@ -1,188 +1,92 @@
 #if 01
 #include <stdio.h>
 #include <string.h>
-#define INF 0x7fff0000
+
 typedef struct {
 	int y;
 	int x;
-}location;
+	int jump;
+}info;
 
-int my, mx;
+int visit[31][210][210];
+int my, mx, K;
+int map[210][210];
 
-char temp[60][60];
-int map[60][60];
-int temp_visit[60][60];
+int hy[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+int hx[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
 int dx[] = { 0, 1, 0, -1 };
 int dy[] = { -1, 0, 1, 0 };
-int min;
 
+info que[1300000];
+int wp, rp;
 
-void P(void)
+int BFS(void)
 {
-	int i, j;
+	info out;
+	int nx, ny, i;
 
-	for (i = 1; i <= my; i++)
-	{
-		for (j = 1; j <= mx; j++)
-		{
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
+	que[wp].y = 1;
+	que[wp].x = 1;
+	que[wp++].jump = 0;
+	visit[0][1][1] = 1;
 
-void dot(int y, int x, int num)
-{
-	int i, j;
-	int nx, ny;
-	int wp, rp;
-	location out;
-	location que[3000] = { 0, };
-	wp = rp = 0;
-
-	que[wp].y = y;
-	que[wp++].x = x;
-	temp_visit[y][x] = 1;
-	map[y][x] = num;
-	while (wp > rp)
+	while (wp>rp)
 	{
 		out = que[rp++];
 
-		for (i = 0; i < 4; i++)
+		for (i = 0; i<4; i++)
 		{
 			nx = out.x + dx[i];
 			ny = out.y + dy[i];
 			if (nx<1 || ny<1 || nx>mx || ny>my) continue;
 
-			if (temp_visit[ny][nx] == 0 && temp[ny][nx] == 'X')
+			if (ny == my && nx == mx) return visit[out.jump][out.y][out.x];
+			if (map[ny][nx] == 0 && visit[out.jump][ny][nx] == 0)
 			{
+				visit[out.jump][ny][nx] = visit[out.jump][out.y][out.x] + 1;
 				que[wp].y = ny;
-				que[wp++].x = nx;
-				temp_visit[ny][nx] = 1;
-				map[ny][nx] = num;
+				que[wp].x = nx;
+				que[wp++].jump = out.jump;
 			}
 		}
-	}
-}
 
-void init(void)
-{
-	int i, j;
-	int num = 1;
-	for (i = 1; i <= my; i++)
-	{
-		for (j = 1; j <= mx; j++)
+		if (out.jump<K)
 		{
-			if (temp_visit[i][j] == 0 && temp[i][j] == 'X')
+			for (i = 0; i<8; i++)
 			{
-				dot(i, j, num);
-				num++;
+				nx = out.x + hx[i];
+				ny = out.y + hy[i];
+				if (nx<1 || ny<1 || nx>mx || ny>my) continue;
+
+				if (ny == my && nx == mx) return visit[out.jump][out.y][out.x];
+				if (map[ny][nx] == 0 && visit[out.jump + 1][ny][nx] == 0)
+				{
+					visit[out.jump + 1][ny][nx] = visit[out.jump][out.y][out.x] + 1;
+					que[wp].y = ny;
+					que[wp].x = nx;
+					que[wp++].jump = out.jump + 1;
+				}
+
 			}
 		}
 	}
+
+	return -1;
 }
 
 
 void input(void)
 {
 	int i, j;
-	scanf("%d %d", &my, &mx);
+	scanf("%d", &K);
+	scanf("%d %d", &mx, &my);
 
 	for (i = 1; i <= my; i++)
 	{
 		for (j = 1; j <= mx; j++)
 		{
-			scanf(" %c", &temp[i][j]);
-		}
-	}
-}
-
-
-
-int BFS(int sy, int sx)
-{
-	int i, j;
-	int di, dj;
-	int nx, ny;
-	int wp, rp;
-	int visit[60][60] = { 0, };
-	location out;
-	location que[3000] = { 0, };
-
-	wp = rp = 0;
-
-	que[wp].y = sy;
-	que[wp++].x = sx;
-	visit[sy][sx] = 0;
-
-	while (wp > rp)
-	{
-		out = que[rp++];
-
-		for (i = 0; i < 4; i++)
-		{
-			nx = out.x + dx[i];
-			ny = out.y + dy[i];
-
-			if (nx<1 || ny<1 || nx>mx || ny>my) continue;
-
-			if (map[ny][nx] == 2)
-			{
-				/*if (visit[out.y][out.x] == 2)
-				{
-				printf("visit = %d\n", visit[out.y][out.x]);
-				for (di = 1; di <= my; di++)
-				{
-				for (dj = 1; dj <= mx; dj++)
-				{
-				visit[que[0].y][que[0].x] = 9;
-				printf("%d ", visit[di][dj]);
-				}
-				printf("\n");
-				}
-				printf("\n");
-				}*/
-				return visit[out.y][out.x];
-			}
-
-			if (map[ny][nx] == 0 && visit[ny][nx] == 0)
-			{
-				que[wp].y = ny;
-				que[wp++].x = nx;
-				visit[ny][nx] = visit[out.y][out.x] + 1;
-			}
-		}
-	}
-
-	return INF;
-}
-
-
-void BFS_all(void)
-{
-	int i, j, k, nx, ny;
-	int num;
-	for (i = 1; i <= my; i++)
-	{
-		for (j = 1; j <= mx; j++)
-		{
-			if (map[i][j] == 1)
-			{
-				for (k = 0; k < 4; k++)
-				{
-					ny = i + dy[k];
-					nx = j + dx[k];
-					if (ny<1 || nx<1 || ny>my || nx>mx) continue;
-					if (map[ny][nx] == 0)
-					{
-						num = BFS(i, j);
-
-						if (num < min) min = num;
-						break;
-					}
-				}
-			}
+			scanf("%d", &map[i][j]);
 		}
 	}
 }
@@ -190,20 +94,18 @@ void BFS_all(void)
 
 int main(void)
 {
+	int sol;
+
 	input();
 
-	init();
 
-	//P();
+	sol = BFS();
 
-	min = INF;
+	printf("%d", sol);
 
-	BFS_all();
-
-	printf("%d", min);
 
 	return 0;
 }
 
-#endif
 
+#endif
