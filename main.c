e1edf87a05afe2cfb4b65b63d342976437e70ec8
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-09 18:45:16 +0900

    Add files via upload
    path finding algorithm

--- main.c
+++ main.c
@@ -1,60 +1,99 @@
 #if 01
 #include <stdio.h>
-#define INF 0x7fff0000
-int N, cost[13][13];
-int min;
-int visit[13];
+#include <string.h>
 
-void DFS(int L, int start, int sum)
+int visit[1010];
+int one[1010][31];
+int que[1010];
+int wp, rp;
+int start, end;
+int N, K;
+
+
+int abs(int num)
 {
-	int i;
-	if (sum >= min) return;
-	if (L == N)
-	{
-		if (cost[start][1] == 0) return;
-		if (sum + cost[start][1] < min) min = sum + cost[start][1];
-		return;
-	}
+	return (num>0) ? num : -num;
+}
 
-	for (i = 2; i <= N; i++)
+
+void BFS(void)
+{
+	int out, i, j;
+	int cnt = 0;
+	wp = rp = 0;
+
+	que[wp++] = start;
+	visit[start] = -1;
+
+	while (wp>rp)
 	{
-		if (visit[i] == 0 && cost[start][i]>0)
+		out = que[rp++];
+
+		for (i = 1; i <= N; i++)
 		{
-			visit[i] = 1;
-			DFS(L + 1, i, sum + cost[start][i]);
-			visit[i] = 0;
+			if (visit[i] != 0 || out == i) continue;
+			cnt = 0;
+
+			for (j = 1; j <= K; j++)
+			{
+				if (one[out][j] != one[i][j]) cnt++;
+				if (cnt == 2) break;
+			}
+
+			if (cnt == 1)
+			{
+				visit[i] = out;
+				if (i == end) return;
+				que[wp++] = i;
+			}
 		}
 	}
 
 }
-
 
 void input(void)
 {
 	int i, j;
-	scanf("%d", &N);
+	int temp;
+	scanf("%d %d", &N, &K);
 
 	for (i = 1; i <= N; i++)
 	{
-		for (j = 1; j <= N; j++)
+		for (j = 1; j <= K; j++)
 		{
-			scanf("%d", &cost[i][j]);
+			scanf("%1d", &one[i][j]);
 		}
 	}
-}
 
+	scanf("%d %d", &start, &end);
+}
 
 int main(void)
 {
-	input();
-	min = INF;
-	visit[1] = 1;
-	DFS(1, 1, 0);
+	int i = 0;
+	int cnt = 1;
+	int best[1010];
 
-	if (min != INF)printf("%d\n", min);
-	else printf("0\n");
+	input();
+
+	BFS();
+
+	if (visit[end] == 0)
+	{
+		printf("-1");
+		return 0;
+	}
+
+	best[i] = end;
+
+	for (;;)
+	{
+		if (visit[best[i]] == -1) break;
+		best[cnt++] = visit[best[i++]];
+	}
+	for (i; i >= 0; i--) printf("%d ", best[i]);
+
 	return 0;
 }
+
 #endif
-
-
