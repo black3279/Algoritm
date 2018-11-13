ac48573c5ae025515b0eb6814243090364d2e588
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-13 09:32:18 +0900

    Add files via upload
    It's a Printer Que Simulation. Printer should print document according to importance and FIFO.

--- main.c
+++ main.c
@@ -1,247 +1,85 @@
 #if 01
 #include <stdio.h>
 #include <string.h>
-
+#define INF 0x7fff0000
 typedef struct {
-	int y;
-	int x;
-	int dir; // dir 은 0이 가로 1이 세로
+	int idx;
+	int rank;
 }info;
 
-info que[1000000];
+info num[1000];
+int cnt;
 
-info start;
-info end; // 큐도 만들기
-int N;
-char map[60][60];
-int visit[2][60][60];
-int wp, rp;
-
-int dx[] = { 0, 1, 0, -1 };
-int dy[] = { -1, 0, 1, 0 };
-
-void map_P(void)
-{
-	int i, j;
-	for (i = 1; i <= N; i++)
-	{
-		for (j = 1; j <= N; j++)
-		{
-			printf("%c ", map[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-
-}
-
-void P(void)
-{
-	int i, j;
-
-	printf("visit[0]\n");
-	for (i = 1; i <= N; i++)
-	{
-		for (j = 1; j <= N; j++)
-		{
-			printf("%2d ", visit[0][i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-
-	printf("visit[1]\n");
-	for (i = 1; i <= N; i++)
-	{
-		for (j = 1; j <= N; j++)
-		{
-			printf("%2d ", visit[1][i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-
-}
-
-int BFS(void)
-{
-	info out;
-	int nx, ny, i, direc;
-	wp = rp = 0;
-
-	que[wp].y = start.y;
-	que[wp].x = start.x;
-	que[wp++].dir = start.dir;
-	visit[start.dir][start.y][start.x] = 1;
-
-
-	while (wp > rp)
-	{
-		out = que[rp++];
-		direc = out.dir;
-		if (out.x == end.x && out.y == end.y && direc == end.dir) return visit[out.dir][out.y][out.x] - 1;
-
-		for (i = 0; i < 4; i++)
-		{
-			nx = out.x + dx[i];
-			ny = out.y + dy[i];
-
-			if (nx<1 || nx>N || ny<1 || ny>N) continue;
-
-			if (nx == end.x && ny == end.y && direc == end.dir) return visit[out.dir][out.y][out.x];
-
-			if (direc == 0 && check_0(ny, nx, i) == 1)
-			{
-				if (visit[0][ny][nx] == 0)
-				{
-					visit[0][ny][nx] = visit[0][out.y][out.x] + 1;
-					que[wp].y = ny;
-					que[wp].x = nx;
-					que[wp++].dir = direc;
-				}
-			}
-			if (direc == 1 && check_1(ny, nx, i) == 1)
-			{
-				if (visit[1][ny][nx] == 0)
-				{
-					visit[1][ny][nx] = visit[1][out.y][out.x] + 1;
-					que[wp].y = ny;
-					que[wp].x = nx;
-					que[wp++].dir = direc;
-				}
-			}
-		}
-		if (rotate(out.y, out.x) == 1 && visit[!out.dir][out.y][out.x] == 0)
-		{
-			visit[!out.dir][out.y][out.x] = visit[out.dir][out.y][out.x] + 1;
-			que[wp].y = out.y;
-			que[wp].x = out.x;
-			que[wp++].dir = !out.dir;
-		}
-	}
-
-
-	return -1;
-
-}
-
-
-int rotate(int y, int x)
-{
-	int i, j;
-	int sx, sy, ex, ey;
-
-	sy = y - 1;
-	sx = x - 1;
-	ey = y + 1;
-	ex = x + 1;
-	if (sy<1 || sx<1 || ey>N || ex>N) return 0;
-
-	for (i = sy; i <= ey; i++)
-	{
-		for (j = sx; j <= ex; j++)
-		{
-			if (map[i][j] == '1') return 0;
-		}
-	}
-
-	return 1;
-}
-
-int check_1(int y, int x, int dir)// _0은 현재 방향이 0일때
-{
-	int i, j, nx, ny;
-	nx = x + dx[dir];
-	ny = y + dy[dir];
-
-	if (dir == 1 || dir == 3)
-	{
-		if (map[y][x] == '0' && map[y - 1][x] == '0' && map[y + 1][x] == '0') return 1;
-		else return 0;
-	}
-
-	if (nx<1 || nx>N || ny<1 || ny>N) return;
-
-	if (dir == 0 || dir == 2)
-	{
-		if (map[ny][nx] == '0') return 1;
-		else return 0;
-	}
-}
-
-//dir은 내가 가려는 방향.
-int check_0(int y, int x, int dir)// _0은 현재 방향이 0일때
-{
-	int i, j, nx, ny;
-	nx = x + dx[dir];
-	ny = y + dy[dir];
-
-	if (dir == 0 || dir == 2)
-	{
-		if (map[y][x] == '0' && map[y][x - 1] == '0' && map[y][x + 1] == '0') return 1;
-		else return 0;
-	}
-
-	if (nx<1 || nx>N || ny<1 || ny>N) return;
-
-	if (dir == 1 || dir == 3)
-	{
-		if (map[ny][nx] == '0') return 1;
-		else return 0;
-	}
-}
+int N, M;
 
 void input(void)
 {
-	int i, j;
-	int sflag = 0, eflag = 0;
-	scanf("%d", &N);
-
-	for (i = 1; i <= N; i++)
+	int i;
+	scanf("%d %d", &N, &M);
+	for (i = 0; i < N; i++)
 	{
-		for (j = 1; j <= N; j++)
+		scanf("%d", &num[i].rank);
+		num[i].idx = i;
+	}
+}
+
+void move_forward(void)
+{
+	int i;
+	info nul = { 0, };
+	for (i = 1; num[i].rank; i++)
+	{
+		num[i - 1] = num[i];
+		num[i] = nul;
+	}
+	return;
+}
+
+int find_end(void)
+{
+	int i, j;
+
+	for (i = 0; num[i].rank; i++);
+
+	return i;
+}
+
+int go_print(void)
+{
+	int i, j;
+	int max;
+	int max_idx;
+	info temp;
+	info nul = { 0, };
+	int now;
+	for (;;)
+	{
+		max = -1;
+		for (j = 0; num[j].rank; j++)
 		{
-			scanf(" %c", &map[i][j]);
-
-			if (map[i][j] == 'B' && sflag == 2) map[i][j] = '0';
-			if (map[i][j] == 'B' && sflag == 1)
+			if (num[j].rank > max)
 			{
-				start.y = i;
-				start.x = j;
-				sflag++;
-				if (map[i][j - 1] == 'B')
-				{
-					start.dir = 0;
-					map[i][j - 1] = '0';
-				}
-				if (map[i - 1][j] == 'B')
-				{
-					start.dir = 1;
-					map[i - 1][j] = '0';
-				}
-				map[i][j] = '0';
+				max = num[j].rank;
+				max_idx = j;
 			}
-			if (map[i][j] == 'B' && sflag == 0) sflag++;
+		}
 
-			if (map[i][j] == 'E' && eflag == 2) map[i][j] = '0';
-			if (map[i][j] == 'E' && eflag == 1)
+		if (max_idx != 0)
+		{
+			temp = num[0];
+			move_forward();
+			num[find_end()] = temp;
+			continue;
+		}
+		if (max_idx == 0)
+		{
+			if (num[0].idx == M) return cnt;
+			else
 			{
-				end.y = i;
-				end.x = j;
-				eflag++;
-				if (map[i][j - 1] == 'E')
-				{
-					end.dir = 0;
-					map[i][j - 1] = '0';
-				}
-				if (map[i - 1][j] == 'E')
-				{
-					end.dir = 1;
-					map[i - 1][j] = '0';
-				}
-				map[i][j] = '0';
+				num[0] = nul;
+				cnt++;
+				move_forward();
 			}
-			if (map[i][j] == 'E' && eflag == 0) eflag++;
 		}
 	}
 }
@@ -249,23 +87,21 @@ void input(void)
 
 int main(void)
 {
+	int T, tc;
 	int sol;
+	scanf("%d", &T);
 
-	input();
+	for (tc = 1; tc <= T; tc++)
+	{
+		cnt = 1;
+		memset(num, 0, sizeof(num));
+		input();
+		sol = go_print();
 
-	sol = BFS();
-
-	//printf("ey = %d, ex = %d, edir = %d\n", end.y, end.x, end.dir);
-
-	//P();
-
-	//map_P();
-
-	if (sol > 0) printf("%d", sol);
-	else printf("0");
+		printf("%d\n", sol);
+	}
 
 	return 0;
 }
-
 
 #endif
