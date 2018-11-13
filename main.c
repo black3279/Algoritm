396e7b8f08e1ccfe6e24556af3d427986b13d9ee
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-13 10:01:08 +0900

    Add files via upload
    shoud calculate when the game will end. It's a snake game, and I used back path tracking way to solve the problem.

--- main.c
+++ main.c
@@ -2,63 +2,160 @@
 #include <stdio.h>
 #include <string.h>
 
-int FU[500000];
-int FD[500000];
-int sum[500000];
-int N, H;
-int half;
-int min, sol;
+struct {
+	int y;
+	int x;
+	int direc;
+	int len;
+}snake;
 
-void find_sol(void)
+typedef struct {
+	int map;
+	int direc;
+}info;
+
+struct {
+	int time;
+	char LD;
+}cmd[110];
+
+info map[110][110];
+int N;
+int K;
+int L;
+int d_cnt;
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { -1, 0, 1, 0 };
+int left[] = { 3, 0, 1, 2 };
+int right[] = { 1, 2, 3, 0 };
+int you[] = { 2, 3, 0, 1 };
+
+void map_P(void)
 {
-	int i;
-	int num = H + 1;
-
-	for (i = 2; i < H; i++)
+	int i, j;
+	for (i = 1; i <= N; i++)
 	{
-		sum[i] = sum[i - 1] - FD[i - 1] + FU[num - i];
-		if (sum[i] == min)
+		for (j = 1; j <= N; j++)
 		{
-			sol++;
+			printf("%d ", map[i][j].map);
 		}
-		if (sum[i] < min)
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void direc_P(void)
+{
+	int i, j;
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j <= N; j++)
 		{
-			min = sum[i];
-			sol = 1;
+			printf("%d ", map[i][j].direc);
 		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void move_apple(int y, int x)
+{
+	map[y][x].map = 1;
+	map[y][x].direc = snake.direc;
+	snake.y = y;
+	snake.x = x;
+	snake.len++;
+	return;
+}
+
+void move_none(int y, int x, int tk)
+{
+	int i, j, nx, ny, sx, sy;
+	info nul = { 0, };
+	int len;
+
+	map[y][x].map = 1;
+	map[y][x].direc = snake.direc;
+	snake.y = y;
+	snake.x = x;
+	len = snake.len;
+	nx = x;
+	ny = y;
+	//printf("move none = %d  len = %d\n", tk, len);
+	for (i = len; i >= 1; i--)
+	{
+		sx = nx, sy = ny;
+		//printf("sy = %d, sx = %d\n", sy, sx);
+		nx = nx + dx[you[map[sy][sx].direc]];
+		ny = ny + dy[you[map[sy][sx].direc]];
+		//printf("ny = %d, nx = %d\n", ny, nx);
+	}
+	//printf("final ny = %d, nx = %d\n", ny, nx);
+	map[ny][nx] = nul;
+
+}
+
+int time_start(void)
+{
+	int tk = 1;
+	int nx, ny;
+	//printf("0\n");
+	//map_P();
+	//direc_P();
+	for (;;)
+	{
+		nx = snake.x + dx[snake.direc];
+		ny = snake.y + dy[snake.direc];
+
+		if (map[ny][nx].map == 1 || nx > N || ny > N || nx < 1 || ny < 1) return tk;
+		if (map[ny][nx].map == 7) move_apple(ny, nx);
+		else if (map[ny][nx].map == 0) move_none(ny, nx, tk);
+
+		if (d_cnt <= L && tk == cmd[d_cnt].time)
+		{
+			if (cmd[d_cnt].LD == 'D') snake.direc = right[snake.direc];
+			if (cmd[d_cnt].LD == 'L') snake.direc = left[snake.direc];
+			d_cnt++;
+		}
+		//printf("%d\n", tk);
+		//map_P();
+		//direc_P();
+		tk++;
 	}
 }
 
 void input(void)
 {
-	int i;
-	int a, b;
-	scanf("%d %d", &N, &H);
-	half = N / 2;
-
-	for (i = 1; i <= half; i++)
+	int i, j;
+	int x, y;
+	snake.y = 1, snake.x = 1, snake.direc = 1, snake.len = 1;
+	d_cnt = 1;
+	scanf("%d", &N);
+	scanf("%d", &K);
+	for (i = 1; i <= K; i++)
 	{
-		scanf("%d", &a);
-		FD[a]++;
+		scanf("%d %d", &y, &x);
+		map[y][x].map = 7;
+	}
+	scanf("%d", &L);
 
-		scanf("%d", &b);
-		FU[b]++;
+	for (i = 1; i <= L; i++)
+	{
+		scanf("%d %c", &cmd[i].time, &cmd[i].LD);
 	}
 
+	map[1][1].map = 1;
+	map[1][1].direc = 1;
 }
 
 int main(void)
 {
+	int sol;
 	input();
 
-	min = half;
-	sol = 2;
-	sum[1] = sum[H] = half;
+	sol = time_start();
 
-	find_sol();
-
-	printf("%d %d", min, sol);
-
+	printf("%d", sol);
 
 	return 0;
 }
