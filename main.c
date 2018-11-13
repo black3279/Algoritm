a5e59160c58f0c0751e14e44ae0b9b0be56aebaf
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-13 09:43:50 +0900

    Add files via upload
    The height information is given. Then, I shoud find max island number, when rain drops N meter. (sol=N).

--- main.c
+++ main.c
@@ -1,105 +1,104 @@
 #if 01
 #include <stdio.h>
 #include <string.h>
-#define INF 0x7fff0000
+
 typedef struct {
-	int idx;
-	int rank;
-}info;
+	int y;
+	int x;
+}location;
 
-info num[1000];
-int cnt;
 
-int N, M;
+location que[110 * 110 * 200];
+int wp, rp;
+int H_max, H_min;
+int N;
+int map[110][110];
+int max;
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { -1, 0, 1, 0 };
 
 void input(void)
 {
-	int i;
-	scanf("%d %d", &N, &M);
-	for (i = 0; i < N; i++)
-	{
-		scanf("%d", &num[i].rank);
-		num[i].idx = i;
-	}
-}
-
-void move_forward(void)
-{
-	int i;
-	info nul = { 0, };
-	for (i = 1; num[i].rank; i++)
-	{
-		num[i - 1] = num[i];
-		num[i] = nul;
-	}
-	return;
-}
-
-int find_end(void)
-{
 	int i, j;
+	int num;
+	scanf("%d", &N);
 
-	for (i = 0; num[i].rank; i++);
+	H_max = -1;
+	H_min = 1000000000;
 
-	return i;
+	for (i = 1; i <= N; i++)
+	{
+		for (j = 1; j <= N; j++)
+		{
+			scanf("%d", &map[i][j]);
+			num = map[i][j];
+			if (num > H_max) H_max = num;
+			if (num < H_min) H_min = num;
+		}
+	}
+
+	H_max++;
+	H_min--;
 }
 
-int go_print(void)
+int BFS(int sy, int sx, int height)
 {
-	int i, j;
-	int max;
-	int max_idx;
-	info temp;
-	info nul = { 0, };
-	int now;
-	for (;;)
+	int i, j, nx, ny;
+	location out;
+	que[wp].y = sy;
+	que[wp++].x = sx;
+	map[sy][sx]--;
+	while (wp > rp)
 	{
-		max = -1;
-		for (j = 0; num[j].rank; j++)
-		{
-			if (num[j].rank > max)
-			{
-				max = num[j].rank;
-				max_idx = j;
-			}
-		}
+		out = que[rp++];
 
-		if (max_idx != 0)
+		for (i = 0; i < 4; i++)
 		{
-			temp = num[0];
-			move_forward();
-			num[find_end()] = temp;
-			continue;
-		}
-		if (max_idx == 0)
-		{
-			if (num[0].idx == M) return cnt;
-			else
+			nx = out.x + dx[i];
+			ny = out.y + dy[i];
+			if (map[ny][nx] == height)
 			{
-				num[0] = nul;
-				cnt++;
-				move_forward();
+				que[wp].y = ny;
+				que[wp++].x = nx;
+				map[ny][nx]--;
+
 			}
 		}
 	}
+
+	return 1;
 }
+
+void BFS_all(void)
+{
+	int i, j, k;
+	int sum = 0;
+
+	for (k = H_max; k > H_min; k--)
+	{
+		sum = 0;
+		for (i = 1; i <= N; i++)
+		{
+			for (j = 1; j <= N; j++)
+			{
+				if (map[i][j] == k) sum += BFS(i, j, k);
+			}
+		}
+		if (sum > max) max = sum;
+	}
+}
+
 
 
 int main(void)
 {
-	int T, tc;
-	int sol;
-	scanf("%d", &T);
+	input();
+	max = -1;
 
-	for (tc = 1; tc <= T; tc++)
-	{
-		cnt = 1;
-		memset(num, 0, sizeof(num));
-		input();
-		sol = go_print();
+	BFS_all();
 
-		printf("%d\n", sol);
-	}
+	printf("%d", max);
+
 
 	return 0;
 }
