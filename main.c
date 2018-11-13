fe0fee5a3f83a97cf405d08cac10c1589869559e
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2018-11-13 10:47:05 +0900

    Add files via upload
    Ladder Manifulation. Should find out minimum depth of solution.
    I used DFS way.

--- main.c
+++ main.c
@@ -1,298 +1,115 @@
 #if 01
 #include <stdio.h>
 #include <string.h>
-int N;
-int dx[] = { 0, 1, 0, -1 };
-int dy[] = { -1, 0, 1, 0 };
-int map[25][25];
-int init_map[25][25];
-int dir[6];
-int max;
-int cnt;
+#define INF 0x7fff0000
 
-void P(void)
-{
-	int i, j;
-
-	for (i = 1; i <= N; i++)
-	{
-		for (j = 1; j <= N; j++)
-		{
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-void Move(int direc)
-{
-	int i, j, ny, nx, sx, sy;
-	int num;
-	int visit[25][25] = { 0, };
-
-	if (direc == 0)
-	{
-		for (i = 2; i <= N; i++)
-		{
-			for (j = 1; j <= N; j++)
-			{
-				if (map[i][j])
-				{
-
-					num = map[i][j];
-					ny = i;
-					nx = j;
-					for (;;)
-					{
-						sy = ny;
-						sx = nx;
-						nx = sx + dx[direc];
-						ny = sy + dy[direc];
-
-						if (ny == 1 && map[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							map[ny][nx] = num;
-
-							break;
-						}
-
-						if (map[ny][nx] == 0) continue;
-
-						if (map[ny][nx] != num || (map[ny][nx] == num && visit[ny][nx] == 1))
-						{
-							map[i][j] = 0;
-							map[sy][sx] = num;
-
-							break;
-						}
-
-						if (map[ny][nx] == num && visit[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							visit[ny][nx] = 1;
-							map[ny][nx] = 2 * map[ny][nx];
-							if (2 * num > max) max = 2 * num;
-							break;
-						}
-
-					}
-				}
-			}
-		}
-	}
-
-	if (direc == 1)
-	{
-		for (j = N - 1; j >= 1; j--)
-		{
-			for (i = 1; i <= N; i++)
-			{
-				if (map[i][j] != 0)
-				{
-					num = map[i][j];
-					ny = i;
-					nx = j;
-					for (;;)
-					{
-						sy = ny;
-						sx = nx;
-						nx = nx + dx[direc];
-						ny = ny + dy[direc];
-						if (nx == N && map[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							map[ny][nx] = num;
-							break;
-						}
-
-						if (map[ny][nx] == 0) continue;
-
-						if (map[ny][nx] != num || (map[ny][nx] == num && visit[ny][nx] == 1))
-						{
-							map[i][j] = 0;
-							map[sy][sx] = num;
-							break;
-						}
-
-
-						if (map[ny][nx] == num && visit[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							visit[ny][nx] = 1;
-							map[ny][nx] = 2 * map[ny][nx];
-							if (2 * num > max) max = 2 * num;
-							break;
-						}
-
-					}
-				}
-			}
-		}
-	}
-
-	if (direc == 2)
-	{
-		for (i = N - 1; i >= 1; i--)
-		{
-			for (j = 1; j <= N; j++)
-			{
-				if (map[i][j])
-				{
-					num = map[i][j];
-					ny = i;
-					nx = j;
-					for (;;)
-					{
-						sy = ny;
-						sx = nx;
-						nx = sx + dx[direc];
-						ny = sy + dy[direc];
-
-						if (ny == N && map[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							map[ny][nx] = num;
-
-							break;
-						}
-
-						if (map[ny][nx] == 0) continue;
-
-						if (map[ny][nx] != num || (map[ny][nx] == num && visit[ny][nx] == 1))
-						{
-							map[i][j] = 0;
-							map[sy][sx] = num;
-
-							break;
-						}
-
-
-						if (map[ny][nx] == num && visit[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							visit[ny][nx] = 1;
-							map[ny][nx] = 2 * map[ny][nx];
-							if (2 * num > max) max = 2 * num;
-							break;
-						}
-
-					}
-				}
-			}
-		}
-	}
-
-	if (direc == 3)
-	{
-		for (j = 2; j <= N; j++)
-		{
-			for (i = 1; i <= N; i++)
-			{
-				if (map[i][j])
-				{
-					num = map[i][j];
-					ny = i;
-					nx = j;
-					for (;;)
-					{
-
-						sy = ny;
-						sx = nx;
-						nx = sx + dx[direc];
-						ny = sy + dy[direc];
-
-						if (nx == 1 && map[ny][nx] == 0)
-						{
-							map[i][j] = 0;
-							map[ny][nx] = num;
-
-							break;
-						}
-
-
-						if (map[ny][nx] == 0) continue;
-
-						if (map[ny][nx] != num || (map[ny][nx] == num && visit[ny][nx] == 1))
-						{
-							map[i][j] = 0;
-							map[sy][sx] = num;
-
-							break;
-						}
-
-						if (map[ny][nx] == num && visit[ny][nx] == 0)
-						{
-							visit[ny][nx] = 1;
-							map[i][j] = 0;
-							map[ny][nx] = 2 * map[ny][nx];
-							if (2 * num > max) max = 2 * num;
-
-							break;
-						}
-
-					}
-				}
-			}
-		}
-	}
-}
-
-
-void DFS(int L)
-{
-	int i;
-	if (L == 6)
-	{
-		memcpy(map, init_map, sizeof(map));
-		for (i = 1; i <= 5; i++)
-		{
-			Move(dir[i]);
-		}
-		return;
-	}
-
-	for (i = 0; i < 4; i++)
-	{
-		dir[L] = i;
-		DFS(L + 1);
-	}
-
-}
+int mx, M, my;
+int radder[40][20];
+int min;
+int flag;
 
 void input(void)
 {
 	int i, j;
-	scanf("%d", &N);
+	int a, b;
+	scanf("%d %d %d", &mx, &M, &my);
 
-	for (i = 1; i <= N; i++)
+	for (i = 1; i <= M; i++)
 	{
-		for (j = 1; j <= N; j++)
-		{
-			scanf("%d", &init_map[i][j]);
-			if (init_map[i][j] > max) max = init_map[i][j];
-		}
+		scanf("%d %d", &a, &b);
+		radder[a][b] = 1;
 	}
 }
+
+int check_sol(void)
+{
+	int i, j;
+	int num;
+	for (i = 1; i <= mx; i++)
+	{
+		num = i;
+		for (j = 1; j <= my; j++)
+		{
+			if (radder[j][num])
+			{
+				num = num + 1;
+				continue;
+			}
+			if (radder[j][num - 1])
+			{
+				num = num - 1;
+				continue;
+			}
+		}
+		if (num != i) return 0;
+	}
+	return 1;
+}
+
+void DFS(int L, int y, int x, int cnt)
+{
+	if (flag) return;
+
+	if (y>my)
+	{
+		y = 1;
+		x = x + 1;
+	}
+
+	if (L == cnt)
+	{
+		if (check_sol()) flag = 1;
+		return;
+	}
+
+	if (x >= mx) return;
+
+	if (radder[y][x - 1] == 0 && radder[y][x] == 0)
+	{
+		radder[y][x] = 1;
+		DFS(L + 1, y + 1, x, cnt);
+		radder[y][x] = 0;
+	}
+
+	DFS(L, y + 1, x, cnt);
+}
+
+int DFS_all(void)
+{
+	int cnt = 1;
+
+	while (cnt <= 3)
+	{
+		DFS(0, 1, 1, cnt);
+		if (flag) break;
+		cnt++;
+	}
+
+	if (flag)
+	{
+		return cnt;
+	}
+
+	return -1;
+}
+
 
 
 
 int main(void)
 {
-	max = -1;
 	input();
+	min = INF;
 
-	DFS(1);
-	/*memcpy(map, init_map, sizeof(map));
-	P();
-	Move(2);
-	P();
-	Move(2);
-	P();
-	Move(3);
-	P();*/
-	printf("%d", max);
+
+	if (check_sol())
+	{
+		printf("0\n");
+		return 0;
+	}
+
+	printf("%d\n", DFS_all());
+
 
 	return 0;
 }
